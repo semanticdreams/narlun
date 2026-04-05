d0ccae711238b31964f274f1850ba157e05ef7b4
semanticdreams <188935636+semanticdreams@users.noreply.github.com>
2026-04-05 14:11:11 +0200

    Default backend logging to debug

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ FRONTEND_ERROR_LOG_PATH = os.path.join(basedir, 'frontend-errors.jsonl')
 PUSH_VAPID_PUBLIC_KEY = ''
 PUSH_VAPID_PRIVATE_KEY = ''
 PUSH_VAPID_SUBJECT = DOMAIN
-LOG_LEVEL = 'INFO'
+LOG_LEVEL = 'DEBUG'
 
 # load local config
 if app_settings := os.environ.get('APP_SETTINGS'):
